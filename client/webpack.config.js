4a84d8319a26467f90f72ff9b4e72712a64a39be
Kole Gasior <jkole822@gmail.com>
2021-02-24 21:28:19 -0500

    create webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,24 @@
+const config = {
+	entry: "./assets/js/index.js",
+	output: {
+		path: __dirname + "/dist",
+		filename: "bundle.js",
+	},
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
+	},
+};
+
+module.exports = config;
