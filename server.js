e6e02b227f7d2a6bd2d7d824ba2571dd4ccf1742
Kole Gasior <jkole822@gmail.com>
2021-02-24 21:26:03 -0500

    change dev port and static dir to client

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("client"));
 
 if (process.env.NODE_ENV === "production") {
 	mongoose.connect(process.env.MONGO_URI);
